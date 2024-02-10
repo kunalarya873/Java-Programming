3001f7b717508a3b3221857640216c5ce3ba9772
kunalarya873 <kunalarya873@gmail.com>
2024-02-10 11:50:23 +0530

    Adding Book

--- initial.java
+++ initial.java
@@ -1,5 +1,5 @@
-class a{
-public static void main(String[] args){
-    System.out.println( "Hello World!");
+class hello{
+    public static void main(String[] args){
+        System.out.println( "Hello World!");
     }
 }
